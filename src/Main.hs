d0bba733d8a00935e141a7486f085e98fbfd91f2
Timothy Humphries <tim@utf8.me>
2014-12-15 18:21:21 -0500

    optParse, FileManip, tidy up

--- src/Main.hs
+++ src/Main.hs
@@ -7,39 +7,57 @@ import Data.List
 import Data.Map (Map)
 import qualified Data.Map as M
 import Data.Maybe
+import Data.Monoid
 import Data.Ord
+import Options.Applicative
 import Safe
 import System.Directory
 import System.Environment
-import System.FilePath.Glob
+import System.FilePath.Find as Find
 import System.IO
-import System.Posix
+import System.Posix as P
 
-main = do
-  args <- getArgs
-  let path = headMay args
-  dupes <- maybe (return [[]]) dups path
+data Options = Options { path :: FilePath
+                       , recursive :: Bool
+                       , delete :: Bool
+                       }
+
+main :: IO ()
+main = execParser (info parser mempty) >>= oldMain
+
+parser = Options <$> argument str (metavar "PATH")
+                 <*> switch (short 'r' <>
+                             long  "recursive" <>
+                             help  "Traverse all subfolders")
+                 <*> switch (short 'd' <>
+                             long  "delete" <>
+                             help  "Delete all duplicates")
+
+oldMain :: Options -> IO ()
+oldMain o = do
+  dupes <- dups o
   let keepers = map (keepWhich compare) dupes
       goners = concatMap snd keepers
   mapM_ putStrLn goners
 
-dups :: FilePath -> IO [[FilePath]]
-dups p = do
-  ls <- globDir1 (compile "*") p
-  files <- filterM doesFileExist ls
-  stats <- mapM getFileStatus files
-  let sizePairs = zipWith (\a b -> (fileSize a, b)) stats files
-      sizeMap = buildMap sizePairs
-      possibleDupes = M.filter (\xs -> length xs > 1) sizeMap
+
+dups :: Options -> IO [[FilePath]]
+dups o = do
+  sizeMap <- Find.fold recPredicate buildMap M.empty (path o)
+  let possibleDupes = M.filter (\xs -> length xs > 1) sizeMap
   groups <- concatMapM checkHashes (map snd $ M.toList possibleDupes)
   return (filter (\ls -> length ls > 1) groups)
+  where recPredicate = return (recursive o)
 
-buildMap :: Ord a => [(a, b)] -> Map a [b]
-buildMap = go M.empty
-  where go m [] = m
-        go m ((x,y):xs) = if M.notMember x m
-                          then go (M.insert x [y] m) xs
-                          else go (M.adjust (y:) x m) xs
+buildMap :: Map FileOffset [FilePath] -> FileInfo -> Map FileOffset [FilePath]
+buildMap m info = case ftyp of
+  RegularFile -> if M.notMember size m
+                 then M.insert size [infoPath info] m
+                 else M.adjust (infoPath info :) size m
+  _           -> m
+  where stat = infoStatus info
+        size = P.fileSize stat
+        ftyp = statusType stat
 
 checkHashes :: [FilePath] -> IO [[FilePath]]
 checkHashes xs = do
@@ -54,10 +72,10 @@ keepWhich ord xs = let keeper = maximumBy ord xs
                    in (keeper, filter (/= keeper) xs)
 
 hashEm :: [FilePath] -> IO [(ByteString, FilePath)]
-hashEm xs = mapM hashFile xs >>= (return . (flip zip) xs)
+hashEm xs = liftM (`zip` xs) (mapM hashFile xs)
 
 hashFile :: FilePath -> IO ByteString
-hashFile fn = B.readFile fn >>= return . hash
+hashFile fn = liftM hash (B.readFile fn)
 
 concatMapM :: (Monad m) => (a -> m [b]) -> [a] -> m [b]
 concatMapM f xs = liftM concat (mapM f xs)
